a3131afefc7cd578ee2f9b59e399509dba11fd26
Chong Yidong <cyd@gnu.org>
2012-03-24 21:37:51 +0800

    csv-mode.el: Improve commentary.

--- csv-mode.el
+++ csv-mode.el
@@ -23,33 +23,42 @@
 
 ;;; Commentary:
 
-;; This package is intended for use with GNU Emacs 21 (only) and
-;; implements the following commands to process records of CSV
-;; (comma-separated value) type: `csv-sort-fields' and
-;; `csv-sort-numeric-fields' sort respectively lexicographically and
-;; numerically on a specified field or column; `csv-reverse-region'
-;; reverses the order.  They are based closely on, and use, code in
-;; `sort.el'.  `csv-kill-fields' and `csv-yank-fields' respectively
-;; kill and yank fields or columns, although they do not use the
-;; normal kill ring.  `csv-kill-fields' can kill more than one field
-;; at once, but multiple killed fields can be yanked only as a fixed
-;; group equivalent to a single field.  `csv-align-fields' aligns
-;; fields into columns; `csv-unalign-fields' undoes such alignment;
-;; separators can be hidden within aligned records.  `csv-transpose'
-;; interchanges rows and columns.  For details, see the documentation
-;; for the individual commands.
+;; This package implements CSV mode, a major mode for editing records
+;; in a generalized CSV (character-separated values) format.  It binds
+;; finds with prefix ".csv" to `csv-mode' in `auto-mode-alist'.
 
-;; CSV mode supports a generalised comma-separated values format
-;; (character-separated values) in which the fields can be separated
-;; by any of several single characters, specified by the value of the
-;; customizable user option `csv-separators'.  CSV data fields can be
-;; delimited by quote characters (and must if they contain separator
-;; characters).  This implementation supports quoted fields, where the
-;; quote characters allowed are specified by the value of the
-;; customizable user option `csv-field-quotes'.  By default, the only
-;; separator is a comma and the only field quote is a double quote.
-;; These user options can be changed ONLY by CUSTOMIZING them,
-;; e.g. via the command `customize-variable'.
+;; In CSV mode, the following commands are available:
+
+;; - C-c C-s (`csv-sort-fields') and C-c C-n (`csv-sort-numeric-fields')
+;;   respectively sort lexicographically and numerically on a
+;;   specified field or column.
+
+;; - C-c C-r (`csv-reverse-region') reverses the order.  (These
+;;   commands are based closely on, and use, code in `sort.el'.)
+
+;; - C-c C-k (`csv-kill-fields') and C-c C-y (`csv-yank-fields') kill
+;;   and yank fields or columns, although they do not use the normal
+;;   kill ring.  C-c C-k can kill more than one field at once, but
+;;   multiple killed fields can be yanked only as a fixed group
+;;   equivalent to a single field.
+
+;; - C-c C-a (`csv-align-fields') aligns fields into columns
+
+;; - C-c C-u (`csv-unalign-fields') undoes such alignment; separators
+;;   can be hidden within aligned records.
+
+;; - C-c C-t (`csv-transpose') interchanges rows and columns.  For
+;;   details, see the documentation for the individual commands.
+
+;; CSV mode can recognize fields separated by any of several single
+;; characters, specified by the value of the customizable user option
+;; `csv-separators'.  CSV data fields can be delimited by quote
+;; characters (and must if they contain separator characters).  This
+;; implementation supports quoted fields, where the quote characters
+;; allowed are specified by the value of the customizable user option
+;; `csv-field-quotes'.  By default, the only separator is a comma and
+;; the only field quote is a double quote.  These user options can be
+;; changed ONLY by customizing them, e.g. via M-x customize-variable.
 
 ;; CSV mode commands ignore blank lines and comment lines beginning
 ;; with the value of the buffer local variable `csv-comment-start',
